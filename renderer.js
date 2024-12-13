50f6da03d09e994b6e356561633f379dc9c15242
masterbtcltc <89700725+masterbtcltc@users.noreply.github.com>
2024-12-13 12:17:16 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -39,10 +39,11 @@ async function fetchPrices() {
     btcPriceElement.textContent = `${addCommas(formattedBtcPrice)} BTC`;
     
     const ltcToBtcRatio = (ltcPrice / btcPrice).toFixed(6); // Calculate LTC to BTC ratio with 6 decimals
-    const btcToLtcRatio = (btcPrice / ltcPrice).toFixed(0); // Calculate BTC to LTC ratio with no decimals
+    const btcToLtcRatio = Math.round(btcPrice / ltcPrice); // Calculate BTC to LTC ratio without decimals
 
     btcToLtcRatioElement.textContent = `${btcToLtcRatio} BTC:LTC`;
     ltcToBtcRatioElement.textContent = `${ltcToBtcRatio} LTC:BTC`;
+
 
 
 
