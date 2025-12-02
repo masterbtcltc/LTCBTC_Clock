ce28d4918d3351b2557cce546a2b31db52183251
masterbtcltc <89700725+masterbtcltc@users.noreply.github.com>
2025-12-02 11:56:45 -0500

    Change DOGE color logic in renderer.js

--- renderer.js
+++ renderer.js
@@ -147,7 +147,7 @@ async function fetchPrices() {
 
     if (lastRatioDOGEtoLTC !== null) {
       dogeLtcElem.style.color =
-        parseFloat(ratioDOGEtoLTC) > parseFloat(lastRatioDOGEtoLTC) ? DOGE_UP_COLOR : DOGE_DOWN_COLOR;
+        parseFloat(ratioDOGEtoLTC) > parseFloat(lastRatioDOGEtoLTC) ? #00A0FF : "yellow";
     }
     lastRatioDOGEtoLTC = ratioDOGEtoLTC;
 
